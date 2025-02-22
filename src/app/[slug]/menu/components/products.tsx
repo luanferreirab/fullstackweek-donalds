fe6ae492166dc592b3ed8fcb1ebbf34160acf1ef
Luan Ferreira <luanbarroso01@hotmail.com.br>
2025-02-21 22:13:26 -0300

    feat: add product details page

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
+import { formatCurrency } from '@/format-currency';
+
 
 interface ProductsProps {
     products: Product[];
@@ -19,10 +21,8 @@ const Products = ({ products }: ProductsProps) => {
                     <h3 className='text-sm font-medium'>{product.name}</h3>
                     <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
                     <p className='pt-3 text-sm font-semibold'>
-                        {new Intl.NumberFormat('pb-BR', { 
-                            style: 'currency', 
-                            currency: 'BRL' 
-                        }).format(product.price)}</p>
+                        {formatCurrency(product.price)}
+                    </p>
                 </div>
                 {/* DIREITA */}
                 <div className="relative min-h-[82px] min-w-[120px]">
