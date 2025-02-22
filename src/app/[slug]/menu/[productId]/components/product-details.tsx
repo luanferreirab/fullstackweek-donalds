b63f51895b30baf2cfb0a79336759b1ac59be19b
Luan Ferreira <luanbarroso01@hotmail.com.br>
2025-02-22 00:03:20 -0300

    feat: add inicial add to cart function to cart context

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -23,7 +23,7 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({product}: ProductDetailsProps) => {
-    const { toggleCart} = useContext(CartContext)
+    const { toggleCart, addProduct} = useContext(CartContext)
     const [quantity, setQuantity] = useState<number>(1)
     const handleDecreaseQuantity = () => {
         setQuantity((prev) => {
@@ -37,6 +37,10 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
         setQuantity((prev) => prev + 1)
     }
     const handleAddToCart = () => {
+        addProduct({
+            ...product,
+            quantity,
+        })
         toggleCart();
     }
     return ( 
--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -10,7 +10,7 @@ import {
 import { CartContext } from "../contexts/cart";
 
 const CartSheet = () => {
-    const { isOpen, toggleCart }  = useContext(CartContext)
+    const { isOpen, toggleCart, products }  = useContext(CartContext)
     return ( 
         <Sheet open={isOpen} onOpenChange={toggleCart}>
             <SheetContent>
@@ -20,6 +20,11 @@ const CartSheet = () => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolorum consequuntur exercitationem deserunt non earum fugit tempora reiciendis eius vero quod animi quibusdam, aliquid nisi adipisci quaerat, libero perspiciatis? Laborum.
                     </SheetDescription>
                 </SheetHeader>
+                {products.map(product => (
+                    <h1 key={product.id}>
+                        {product.name} - {product.quantity}
+                    </h1>
+                ))}
             </SheetContent>
         </Sheet>
      );
