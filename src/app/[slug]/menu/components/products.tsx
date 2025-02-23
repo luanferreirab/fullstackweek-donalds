f53b229764ad98a35a8a7e67881696d34dd5148b
Luan Ferreira <luanbarroso01@hotmail.com.br>
2025-02-23 18:02:30 -0300

    feat: ensure order can be created

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,7 +1,7 @@
 import { Product } from '@prisma/client';
 import Image from "next/image";
 import Link from 'next/link';
-import { useParams } from 'next/navigation';
+import { useParams, useSearchParams } from 'next/navigation';
 
 import { formatCurrency } from '@/format-currency';
 
@@ -12,10 +12,15 @@ interface ProductsProps {
 
 const Products = ({ products }: ProductsProps) => {
     const {slug} = useParams<{ slug: string }>();
+    const searchParams = useSearchParams();
+    const consumptionMethod = searchParams.get("consumptionMethod")
     return (
     <div className='space-y-3 px-5'>
         {products.map(product => (
-            <Link key={product.id} href={`/${slug}/menu/${product.id}`} className="flex items-center justify-between gap-10 py-3 border-b">
+            <Link 
+                key={product.id} 
+                href={`/${slug}/menu/${product.id}?consumptionMethod=${consumptionMethod}`} 
+                className="flex items-center justify-between gap-10 py-3 border-b">
                 {/* ESQUERDA */}
                 <div>
                     <h3 className='text-sm font-medium'>{product.name}</h3>
--- src/app/[slug]/menu/helpers/cpf.ts
+++ src/app/[slug]/menu/helpers/cpf.ts
@@ -1,3 +1,7 @@
+export const removeCpfPunctuation = (cpf: string) => {
+    return cpf.replace(/[\.\-]/g, "")
+}
+
 export const isValidCpf = (cpf: string): boolean => {
     // remove caracteres não numericos
     cpf = cpf.replace(/\D/g, "");
